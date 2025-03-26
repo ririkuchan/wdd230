39871ff680091da9b0673a5501f8900de746c061
ririkuchan <ririkuchan@gmail.com>
2025-03-27 05:21:37 +0900

    DOM Manipulation

---
+++ week02/scripts/bom.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    const chapter = input.value.trim();
+
+    if (chapter !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = chapter;
+        deleteButton.textContent = '❌';
+        deleteButton.setAttribute('aria-label', `Remove ${chapter}`);
+
+        li.append(deleteButton);
+        list.appendChild(li);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+        input.focus();
+    } else {
+        input.focus();
+    }
+});
